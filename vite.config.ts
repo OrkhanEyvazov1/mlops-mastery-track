0b48386c85b2f968d4e4a9874df6491ad0856cdd
Orkhan Eyvazov <orkhane50616@sabah.edu.az>
2026-02-22 18:13:07 +0400

    Add base path for deployment
    Set the base path for the Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/mlops-mastery-track/',
+
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
@@ -17,6 +17,5 @@ export default defineConfig({
     },
   },
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
